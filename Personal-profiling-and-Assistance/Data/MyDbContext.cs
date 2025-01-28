d050b3e303a3496224439e58572952e073f4e268
HussienWagdy220 <hussienwagdy220@gmail.com>
2025-01-28 12:00:00 +0200

    Create EntityFrameWork and ConnectionString

---
+++ Personal-profiling-and-Assistance/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Personal_profiling_and_Assistance.Data
+{
+    public class MyDbContext : DbContext // to use entity framework 
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)  // ask mena what is this code 
+        { 
+        
+        }
+
+    }
+}
--- Personal-profiling-and-Assistance/Program.cs
+++ Personal-profiling-and-Assistance/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Personal_profiling_and_Assistance.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MyDbContext>(op => 
+    op.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
+
 
 var app = builder.Build();
 
