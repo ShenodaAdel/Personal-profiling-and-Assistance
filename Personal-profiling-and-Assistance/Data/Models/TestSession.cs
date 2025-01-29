4cd1d3dcd9b2a79a052ff36a68057419605a51b7
HussienWagdy220 <hussienwagdy220@gmail.com>
2025-01-29 09:12:41 +0200

    check it error

--- Personal-profiling-and-Assistance/Data/Models/TestSession.cs
+++ Personal-profiling-and-Assistance/Data/Models/TestSession.cs
@@ -1,8 +1,13 @@
-﻿namespace Personal_profiling_and_Assistance.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal_profiling_and_Assistance.Data.Models
 {
     public class TestSession
     {
-        // date start and end 
+        [Key]
+        public int Id { get; set; }
+
+        // date start and end  
         // forign key userid  and reference TestID
     }
 }
